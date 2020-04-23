588f47f76770e231d03adcb3066f522e454d0c44
Yang Cheng <yangchengemail@126.com>
2020-04-23 12:34:23 +0800

    Add rplidar_ros package

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/pi/MyLocalGit/snowbird/build/gtest/cmake_install.cmake")
+  include("/home/pi/MyLocalGit/snowbird/build/rplidar_ros/cmake_install.cmake")
 
 endif()
 
