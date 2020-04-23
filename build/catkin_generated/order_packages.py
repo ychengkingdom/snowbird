d875dfb7c55c93061e671d867981446cf7f9f0e1
Yang Cheng <yangchengemail@126.com>
2020-04-23 11:44:11 +0800

    Creat the catkin workspace for snowbird

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/pi/MyLocalGit/snowbird/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/pi/snowbird/devel;/home/pi/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/pi/snowbird/devel;/home/pi/catkin_ws/devel;/opt/ros/melodic' != '' else []
